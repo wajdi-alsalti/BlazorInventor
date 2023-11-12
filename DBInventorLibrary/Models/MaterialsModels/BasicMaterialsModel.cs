958a2b7101164ff5b7fba83a7a602feb72b8bd68
Wajdi Alsalti <wajdialsalti@gmail.com>
2023-11-12 17:04:33 +0100

    Material models for DB

---
+++ DBInventorLibrary/Models/MaterialsModels/BasicMaterialsModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DBInventorLibrary.Models.MaterialsModels
+{
+    public class BasicMaterialsModel
+    {
+        [BsonId]
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int SapNumber { get; set; }
+
+        public BasicMaterialsModel()
+        {
+
+        }
+        public BasicMaterialsModel(MaterialsModel material)
+        {
+            Id = material.Id;
+            Name = material.Name;
+            SapNumber = material.SapNumber;
+        }
+    }
+}
---
+++ DBInventorLibrary/Models/MaterialsModels/MaterialsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DBInventorLibrary.Models.MaterialsModels
+{
+    public class MaterialsModel
+    {
+        [BsonId]
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        //public string ObjectIdentifer { get; set; }
+        public string Name { get; set; }
+        public int SapNumber { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+    }
+}
