d22e65e18d859552c35437488e02aabf36c24064
Wajdi Alsalti <wajdialsalti@gmail.com>
2023-11-12 17:04:14 +0100

    controller model for DB

---
+++ DBInventorLibrary/Models/ControllerModels/ControllerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DBInventorLibrary.Models.ControllerModels
+{
+    public class ControllerModel
+    {
+        [BsonId]
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string ObjectIdentifer { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
