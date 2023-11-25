15ff30badbf6aa92c2003dd01716557b779cdd57
Wajdi Alsalti <wajdialsalti@gmail.com>
2023-11-25 15:55:28 +0100

    add toast model to be more controlled

---
+++ BlazorInventor/Model/ToastModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorInventor.Model
+{
+    public class ToastModel
+    {
+        public string ToastHeader { get; set; } = "Toast Header";
+        public string ToastMessage { get; set; } = "Toast Message";
+        public string ToastBG { get; set; } = "success";
+        public int Delay { get; set; } = 3000;
+        public string Position { get; set; } = "top-0 end-0";
+    }
+}
