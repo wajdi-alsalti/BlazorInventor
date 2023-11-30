1b3b7c786e1c83a353bfb2c72d2fff92fd5b1a77
Wajdi Alsalti <wajdialsalti@gmail.com>
2023-11-30 11:01:39 +0100

    survey test

---
+++ BlazorInventor/Model/SurveyQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorInventor.Model
+{
+    public class SurveyQuestion
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public int? NumericResponse { get; set; } // Nullable int to handle unanswered questions
+    }
+}
