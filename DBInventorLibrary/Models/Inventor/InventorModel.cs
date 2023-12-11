e7eb1391e418a4d3ba2d999c87483d9675d5ae91
Wajdi Alsalti <wajdialsalti@gmail.com>
2023-12-11 12:07:10 +0100

    new model for items inventor have the final info of the items after inventor

---
+++ DBInventorLibrary/Models/Inventor/InventorModel.cs
@@ -0,0 +1,19 @@
+﻿using DBInventorLibrary.Models.ControllerModels;
+using DBInventorLibrary.Models.MaterialsModels;
+
+namespace DBInventorLibrary.Models.Inventor
+{
+    public class InventorModel
+    {
+        [BsonId]
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public InventorControllers Controllers { get; set; }
+        public int BandNumber { get; set; }
+        public int WagenPosition { get; set; }
+        public string WagenName { get; set; }
+        public List<MaterialsModel> MaterialsControllerd { get; set; }
+        public bool HasBeenControllerd { get; set; }
+    }
+}
