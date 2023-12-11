f06ff83fc08ffc18a2bce1f9310b62404b5b92a1
Wajdi Alsalti <wajdialsalti@gmail.com>
2023-12-11 12:05:11 +0100

    add new data for the final inventor for the items

---
+++ DBInventorLibrary/DataAccess/InventorData/InventorMaterials/IInventorInformation.cs
@@ -0,0 +1,13 @@
+﻿using DBInventorLibrary.Models.Inventor;
+
+namespace DBInventorLibrary.DataAccess.InventorData.InventorMaterials
+{
+    public interface IInventorInformation
+    {
+        Task AddMaterialsAfterInventor(InventorModel materials);
+        Task DeleteInventorMaterials(InventorModel materials);
+        Task<InventorModel> GetMaterials(string Id);
+        Task<List<InventorModel>> GetmaterialsAsync();
+        Task UpadteMaterialsAfterInventor(InventorModel materials);
+    }
+}
