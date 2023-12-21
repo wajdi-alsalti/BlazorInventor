d1759007d89a745870b43049a2e660455c0d106b
Wajdi Alsalti <wajdialsalti@gmail.com>
2023-12-21 17:14:24 +0100

    register the new abstract classes that handle the logic of data base without duplicate code

--- BlazorInventor/RegisterServices.cs
+++ BlazorInventor/RegisterServices.cs
@@ -5,6 +5,18 @@ using DBInventorLibrary.DataAccess.InventorData;
 using DBInventorLibrary.DataAccess.InventorData.InventorMaterials;
 using DBInventorLibrary.DataAccess.Materials;
 using DBInventorLibrary.DataAccess.Wagens;
+using DBInventorLibrary.DataAccessAbstract.Bands;
+using DBInventorLibrary.DataAccessAbstract.Controllers;
+using DBInventorLibrary.DataAccessAbstract.InventorData.Controllers;
+using DBInventorLibrary.DataAccessAbstract.InventorData.Materials;
+using DBInventorLibrary.DataAccessAbstract.Materilas;
+using DBInventorLibrary.DataAccessAbstract.Services;
+using DBInventorLibrary.DataAccessAbstract.Wagens;
+using DBInventorLibrary.Models.Bands;
+using DBInventorLibrary.Models.ControllerModels;
+using DBInventorLibrary.Models.Inventor;
+using DBInventorLibrary.Models.MaterialsModels;
+using DBInventorLibrary.Models.WagensModel;
 
 namespace BlazorInventor
 {
@@ -26,6 +38,13 @@ namespace BlazorInventor
             builder.Services.AddSingleton<IInventorControllerData, InventorControllerData>();
             builder.Services.AddSingleton<IInventorInformation, InventorInformation>();
 
+            // test abstract class
+            builder.Services.AddSingleton<IDataBase<BandsModel>, BandsAbstract>();
+            builder.Services.AddSingleton<IDataBase<ControllerModel>, ControllersAbstract>();
+            builder.Services.AddSingleton<IDataBase<MaterialsModel>, MaterialsAbstract>();
+            builder.Services.AddSingleton<IDataBase<SingleWagenModel>, WagensAbstract>();
+            builder.Services.AddSingleton<IDataBase<InventorModel>, InventorMaterialsDataAbstract>();
+            builder.Services.AddSingleton<IDataBase<InventorControllers>, InventorControllersDataAbstract>();
 
             // for localization
 
